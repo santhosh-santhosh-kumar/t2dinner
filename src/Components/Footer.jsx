2025ae6eff72f045ef5686ddcbb7a3940d7f0934
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2025-03-28 21:56:10 +0530

    footer implement

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import MainFooter from './Footer/MainFooter'
 import MenuFooter from './Footer/MenuFooter'
 import ConductFooter from './Footer/ConductFooter'
+import CopyRight from './Footer/CopyRight'
 
 const Footer = () => {
   return (
-    <div className='bg-[#010f1c] px-10 lg:flex lg:justify-around  py-10  lg:mt-0 mt-4'>
-      <MainFooter />
-      <MenuFooter />
-      <ConductFooter />
-    </div>
+   <> <div className='bg-[#010f1c] px-10 lg:flex lg:justify-around  py-10  lg:mt-0 mt-4'>
+   <MainFooter />
+   <MenuFooter />
+   <ConductFooter />
+ </div>
+ <CopyRight /></>
   )
 }
 
---
+++ src/Components/Footer/CopyRight.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CopyRight = () => {
+  return (
+   <div className='bg-themeRed  text-white font-mainFont lg:flex justify-between py-4 px-10'>
+       <div className='lg:py-0 py-2'>
+      © All Copyright 2024 by T2Dinner
+    </div>
+    <div className='lg:flex items-center gap-2'>
+      <p>Design And Developed by :</p>
+      <p className='text-themeYellow'>Santhosh : 93 6 111 6 112</p></div>
+   </div>
+  )
+}
+
+export default CopyRight
